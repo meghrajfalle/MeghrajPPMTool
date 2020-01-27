d11df8236825abaa1e18af43f50fc34210aaaa79
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-01-27 22:48:12 +0530

    Branch3 commit

---
+++ PPMToolFullStack/src/main/java/com/meghraj/ppmtool/services/ProjectService.java
@@ -0,0 +1,22 @@
+package com.meghraj.ppmtool.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meghraj.ppmtool.domain.Project;
+import com.meghraj.ppmtool.repositories.ProjectRepository;
+
+
+@Service
+public class ProjectService {
+
+  @Autowired
+  private ProjectRepository projectRepository;
+
+  public Project saveOrUpdateProject(Project project){
+
+      //Logic
+      return projectRepository.save(project);
+  }
+
+}
