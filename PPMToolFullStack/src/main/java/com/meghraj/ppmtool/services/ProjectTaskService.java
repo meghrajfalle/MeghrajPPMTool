4f51ad0ab3cd66e429c8c0dfe885bbd9c8bf86cc
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-02-04 21:38:59 +0530

    Branch 34: Persisting the ProjectTask into the Backlog. Completed ProjectTaskService. Created BacklogController

--- PPMToolFullStack/src/main/java/com/meghraj/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/meghraj/ppmtool/services/ProjectTaskService.java
@@ -3,6 +3,7 @@ package com.meghraj.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.meghraj.ppmtool.domain.Backlog;
 import com.meghraj.ppmtool.domain.ProjectTask;
 import com.meghraj.ppmtool.repositories.BacklogRepository;
 import com.meghraj.ppmtool.repositories.ProjectTaskRepository;
@@ -16,17 +17,36 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        //Exceptions: Project not found
 
         //PTs to be added to a specific project, project !=null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
         //set the BL to pt
+        projectTask.setBacklog(backlog);
 
         //We want our project sequence to be like this: IDPRO-1 IDPRO-2  .... 100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+
         // Update the BL SEQUENCE
+        BacklogSequence++;
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         //INITIAL priority when priority null
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+//            projectTask.setPriority(3);
+//        }
+
         //INITIAL status when status is null
-        return null;
+        if(projectTask.getStatus() =="" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
     }
 
 }
