283f84d13e3d43c527cf23ddd223e01ae61a42c8
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-02-08 19:57:41 +0530

    Branch 45: ProjectBoard.js and AddProjectTask.js setup and routes to it in App.js

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -10,6 +10,8 @@ import { Provider } from "react-redux";
 // this is a part that allows us to hook up react with redux
 //the provider is basically how we define our store that we're going to use for our application
 import store from "./store";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
         </div>
       </Router>
     </Provider>
