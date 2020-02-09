1e63fb8ed52b7a7aa3c24044683ae7f8a3b27fcc
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-02-09 11:56:53 +0530

    Branch 51:  Load Project Tasks to UI on the Project Board (Backlog.js) and (ProjectTasks.js)

--- ppmtool-react-client/src/Components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/Components/ProjectBoard/ProjectBoard.js
@@ -15,6 +15,7 @@ class ProjectBoard extends Component {
 
   render() {
     const { id } = this.props.match.params;
+    const { project_tasks } = this.props.backlog;
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -22,17 +23,18 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
-        <Backlog />
+        <Backlog project_tasks_props={project_tasks} />
       </div>
     );
   }
 }
-// ProjectBoard.propTypes = {
-//   backlog: PropTypes.object.isRequired,
-//   getBacklog: PropTypes.func.isRequired
-// };
-// const mapStateToProps = state => ({
-//   backlog: state.backlog
-// });
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+  //If a mapStateToProps function is specified, the new wrapper component will subscribe to Redux store updates.
+  backlog: state.backlog
+});
 
-export default connect(null, { getBacklog })(ProjectBoard);
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
