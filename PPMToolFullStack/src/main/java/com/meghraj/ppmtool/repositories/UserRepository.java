6d8d5d8b44f6660d53be7eac6f4613046b20a123
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-02-10 12:08:15 +0530

    Branch 64: JWT Provider pre-work. CustomUserDetailsService, SecurityConstants, LoginRequest, JWTLoginSuccessResponse new classes created

--- PPMToolFullStack/src/main/java/com/meghraj/ppmtool/repositories/UserRepository.java
+++ PPMToolFullStack/src/main/java/com/meghraj/ppmtool/repositories/UserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.meghraj.ppmtool.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User getById (Long id);
+
 }
