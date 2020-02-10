8ee731c99a351b6e1cb38d8c61ec7903400670fa
Meghraj Falle <meghraj.falle@PUMFALLE.local>
2020-02-10 12:54:03 +0530

    Branch 65: Token Generatedgit diff src/main/java/com/meghraj/ppmtool/web/UserController.java../ppmtool-react-client/src/Components/ProjectBoard/ProjectTasks/UpdateProjectTask.js First Milestone. JwtTokenProvider and /login api Call created in UserController

--- PPMToolFullStack/src/main/java/com/meghraj/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/com/meghraj/ppmtool/web/UserController.java
@@ -1,10 +1,16 @@
 package com.meghraj.ppmtool.web;
 
+import static com.meghraj.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meghraj.ppmtool.domain.User;
+import com.meghraj.ppmtool.payload.JWTLoginSuccessResponse;
+import com.meghraj.ppmtool.payload.LoginRequest;
+import com.meghraj.ppmtool.security.JwtTokenProvider;
 import com.meghraj.ppmtool.services.MapValidationErrorService;
 import com.meghraj.ppmtool.services.UserService;
 import com.meghraj.ppmtool.validator.UserValidator;
@@ -30,6 +39,29 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public  ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX +tokenProvider.generateToken(authentication);
+
+        return  ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
 
